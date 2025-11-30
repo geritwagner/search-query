40de3287422eb093fb692403522249a37b0c267d
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2025-11-30 11:01:16 +0100

    extract PKG_TREE

---
+++ docs/gui/search_query_pkg_tree.js
@@ -0,0 +1,79 @@
+// directory tree of the search_query package (mirrors your existing setup)
+window.searchQueryPKG_TREE = {
+  "": [
+    "__init__.py",
+    "__version__.py",
+    "cli.py",
+    "constants.py",
+    "database.py",
+    "database_queries.py",
+    "exception.py",
+    "linter.py",
+    "linter_base.py",
+    "parser.py",
+    "parser_base.py",
+    "query.py",
+    "query_and.py",
+    "query_or.py",
+    "query_near.py",
+    "query_not.py",
+    "query_range.py",
+    "query_term.py",
+    "registry.py",
+    "search_file.py",
+    "serializer_base.py",
+    "serializer_structured.py",
+    "translator_base.py",
+    "upgrade.py",
+    "utils.py"
+  ],
+  ebscohost: [
+    "__init__.py",
+    "constants.py",
+    "linter.py",
+    "parser.py",
+    "serializer.py",
+    "translator.py"
+  ],
+  "ebscohost/v_1": [
+    "__init__.py",
+    "parser.py",
+    "serializer.py",
+    "translator.py"
+  ],
+  "ebscohost/v_1": ["__init__.py"],
+  generic: ["__init__.py", "linter.py", "serializer.py"],
+  "generic/v_1": ["__init__.py", "serializer.py"],
+  json_db: [
+    "ais_senior_scholars_basket.json",
+    "ais_senior_scholars_list_of_premier_journals.json",
+    "blocks_bmi_343.json",
+    "journals_FT50.json"
+  ],
+  pre_notation: ["__init__.py"],
+  structured: ["__init__.py"],
+  pubmed: [
+    "__init__.py",
+    "constants.py",
+    "linter.py",
+    "parser.py",
+    "serializer.py",
+    "translator.py"
+  ],
+  "pubmed/v_1": [
+    "__init__.py",
+    "parser.py",
+    "serializer.py",
+    "translator.py"
+  ],
+  wos: [
+    "__init__.py",
+    "constants.py",
+    "linter.py",
+    "parser.py",
+    "serializer.py",
+    "translator.py"
+  ],
+  "wos/v_0": ["__init__.py", "parser.py", "serializer.py", "translator.py"],
+  "wos/v_1": ["__init__.py", "parser.py", "serializer.py", "translator.py"]
+};
